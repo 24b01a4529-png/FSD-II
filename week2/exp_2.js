4b2ec4f8e13e660ed810d19effadec9d5bcec0d0
Hansika <hansikadonga@gmail.com>
2026-07-21 12:19:58 +0530

    week 2

---
+++ week2/exp_2.js
@@ -0,0 +1,7 @@
+"use strict";
+const multiply = (a, b) => {
+    return a * b;
+};
+const square = (n) => n * n;
+console.log("Multiplication:", multiply(5, 4));
+console.log("Square:", square(6));
---
+++ week2/exp_2.ts
@@ -0,0 +1,8 @@
+const multiply = (a: number, b: number): number => {
+    return a * b;
+};
+
+const square = (n: number): number => n * n;
+
+console.log("Multiplication:", multiply(5, 4));
+console.log("Square:", square(6));
